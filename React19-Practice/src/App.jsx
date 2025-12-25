04a2344a6a008b66febce970039119f2fd97cd7f
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2025-12-25 20:31:07 +0500

    Es6+

--- React19-Practice/src/App.jsx
+++ React19-Practice/src/App.jsx
@@ -1,14 +1,27 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
+  const item = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+    const styles = {
+        backgroundColor: "lightblue",
+        color: "darkblue",
+        height: "48px",
+        width: "200px",
+        textAlign:"center",
+        alignItems:"center",
+        paddingTop:"15px"
+    }
   return (
-   <>
-    <input type="text" placeholder='Name plz'/>
-   </>
-  )
+    <>
+      <input type="text" placeholder="Name plz" />
+      <p style={styles}>Items: {item.length}</p>
+      <ul>
+        {item.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
-export default App
+export default App;
