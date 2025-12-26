fa36a00dad709706fdd7133531a44d60157862d5
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2025-12-26 22:16:01 +0500

    cc

--- React19-Practice/src/App.jsx
+++ React19-Practice/src/App.jsx
@@ -1,26 +1,42 @@
 import "./App.css";
 
+//Component Creation
+  const Hello = () => {
+    return (
+      <>
+        <h2>Hello World</h2>
+      </>
+    );
+  };
+//Component Creation with Props
+const Greetings = ({name, age}) => {
+  return (
+    <>
+      <h2>Hello {name}, you are {age} years old</h2>
+    </>
+  );
+};
+
+
 function App() {
   const item = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
-    const styles = {
-        backgroundColor: "lightblue",
-        color: "darkblue",
-        height: "48px",
-        width: "200px",
-        textAlign:"center",
-        alignItems:"center",
-        paddingTop:"15px"
-    }
+  const styles = {
+    backgroundColor: "lightblue",
+    color: "darkblue",
+    height: "48px",
+    width: "200px",
+    textAlign: "center",
+    alignItems: "center",
+    paddingTop: "15px",
+  };
 
-  const Hello =() => {
-      return (
-        <>
-        <h2>Hello World</h2>
-      </>);
-    }
   return (
     <>
       <Hello />
+      <Hello />
+      <Hello />
+      <Greetings name="Alice" age={25} />
+      
       <input type="text" placeholder="Name plz" />
       <p style={styles}>Items: {item.length}</p>
       <ul>
